c7a36696a1cba33bb6ecf1278d194bbc9d311199
Brayan-A <brayanayol55@gmail.com>
2020-07-25 12:34:51 -0500

    Project Structure added

---
+++ src/components/exampleComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/test_style.css'
+
+class ExampleComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 className="title_banner" >Hola quetal</h1>
+            </div>
+        )
+    }
+}
+
+export default ExampleComponent;
---
+++ src/pages/testPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExampleComponent from '../components/exampleComponent'
+
+class TestPage extends React.Component {
+    render(){
+        return(
+            <ExampleComponent></ExampleComponent>
+            )
+    }
+}
+
+export default TestPage;
