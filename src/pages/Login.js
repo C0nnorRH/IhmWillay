8b98a1f2b7aa6cbe67c9a963063aa9e9fc713ec6
AlexanderRequelme <alexander.requelme@protonmail.com>
2020-08-13 15:02:58 -0500

    login2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,20 +2,28 @@ import React from 'react'
 import Header from '../components/header'
 import { Component } from 'react'
 import "../styles/login_style.css"
+import logo_profile from "../profile.png"
 
 class Login extends React.Component{
  render(){
-     return(<>
-        <Header/>
+     return(
+     <div className="container">
+        <h1>Willay</h1>
         <div className="login_box_primary">
             <div className="login_box_secundary">
+                <img className="img_sigin" src={logo_profile} width="128" height="128"/>  
+            
                 <input placeholder="Correo Electronico"></input>
                 <input placeholder="Contraseña"></input>
-                <button>Iniciar Sesión</button>
+                <button className="button_sigin">Iniciar Sesión</button>
             </div>
-            <button>Registrarse</button>
+            <div>
+                <label>New to Willay?</label>
+                <a className="button_register">Registrarse</a>
+            </div>            
         </div>        
-     </>)
+     </div>
+     )
  }
 }
 
