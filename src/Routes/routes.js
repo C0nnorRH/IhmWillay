b4ac2e570399702ed155e2c7f09857abbcd64986
AlexanderRequelme <alexander.requelme@protonmail.com>
2020-08-13 16:48:35 -0500

    Login_Register

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import  Home  from '../pages/Home';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 export default class Routes extends React.Component {
@@ -13,6 +14,7 @@ export default class Routes extends React.Component {
               <Redirect to="/Home" />
             </Route>
             <Route exact path="/Login" component={Login} />
+            <Route exact path="/Signup" component={Register} />
           </Switch>
         </div>
     );
--- src/components/block_register.js
+++ src/components/block_register.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import logo_profile from "../profile.png"
-const block_register = () =>{
+import { Link } from 'react-router-dom';
+
+const block_register = (props) =>{
     return(
         <div className="login_box_primary">
             <img className="img_sigin" src={logo_profile} width="128" height="128"/>  
             <div className="login_box login_box_secundary">
-                <input placeholder="Correo Electronico"></input>
-                <input placeholder="Contraseña"></input>
-                <button className="button_sigin">Iniciar Sesión</button>
+                {
+                    props.login == 0 ?
+                        <>
+                            <input placeholder="Correo Electronico"></input>
+                            <input placeholder="Contraseña" type="password"></input>
+                            <button className="button_sigin">Iniciar Sesión</button>
+                        </> 
+                    :
+                    <>
+                        <input placeholder="Nombre"></input>
+                        <input placeholder="Apellido"></input>
+                        <input placeholder="Correo Electronico"></input>
+                        <input placeholder="Contraseña" type="password"></input>
+                        <input placeholder="Confirmar contraseña" type="password"></input>
+                        <button className="button_sigin">Registrarse</button>
+                    </>
+                }
             </div>
-            <div className="login_box login_box_secundary2">
-                <label>New to Willay?</label>
-                <a className="button_register">Registrarse</a>
-            </div>            
+            {
+                props.login == 0?
+                    <div className="login_box login_box_secundary2">
+                        <label>New to Willay?</label>
+                        <Link to='/signup' className="button_register"> Registrarse</Link> 
+                    </div>
+                : null 
+            }
+                       
         </div> 
     )
 }
