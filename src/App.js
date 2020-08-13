d3a4c2efc9cceff347717186a84e10fad5ab96fe
AlexanderRequelme <alexander.requelme@protonmail.com>
2020-08-13 12:12:43 -0500

    Login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home/>      
+      <Login/>      
     </div>
   );
 }
